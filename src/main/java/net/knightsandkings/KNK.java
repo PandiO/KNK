4a579a9fcdb46063fae6215467785634c1a0a19d
Pandi Oldenzeel <pandi.oldenzeel@hotmail.nl>
2020-11-03 19:50:04 +0100

    Created Maven project

---
+++ src/main/java/net/knightsandkings/KNK.java
@@ -0,0 +1,11 @@
+package net.knightsandkings;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KNK extends JavaPlugin
+{
+	public void onEnable()
+	{
+		this.getLogger().info("gsaklghjaletjhgvmbansGLTJN KNK ENABLED");
+	}
+}
